bad8e9199538e355122ada293320babfa00fa568
Chrissan Sy <sy.chrissan@gmail.com>
2014-04-01 20:27:15 +0800

    wave number fix

--- scripts/main.js
+++ scripts/main.js
@@ -431,12 +431,12 @@ var main = function () {
             cd_timesRun = 0;
             waveCountDown = setInterval(function () {
                 cd_timesRun += 1;
-                if (cd_timesRun === wave_delay) {
+                if (cd_timesRun === wave_delay - 1) {
                     clearInterval(waveCountDown);
                     $('#text-overlay-top').html('Wave: ' + GLOBALS.map.wave.current);
                     
                     $('#text-overlay-center').css('font-size', '42px');
-                    $('#text-overlay-center').html('<span>Incoming! Wave #' + GLOBALS.map.wave.current + '</span><br><span style="font-style: italic; font-weight: normal;">&quot;' + waves[GLOBALS.map.wave.current][0] + '&quot;</span>');
+                    $('#text-overlay-center').html('<span>Incoming! Wave #' + (GLOBALS.map.wave.current + 1) + '</span><br><span style="font-style: italic; font-weight: normal;">&quot;' + waves[GLOBALS.map.wave.current][0] + '&quot;</span>');
                     $('#text-overlay-center').animate({
                         opacity: 1,
                         fontSize: '26px'
