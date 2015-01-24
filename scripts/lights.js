4c7fafe9b9d6915eeb0f2b6bc1d9817305e95204
Chrissan Sy <sy.chrissan@gmail.com>
2015-01-24 17:25:41 +0800

    fixed crash caused by non-finite parameter

--- scripts/lights.js
+++ scripts/lights.js
@@ -31,23 +31,27 @@ var LIGHTING = (function () {
         },
         lightenGradient : function (ctx, x, y, radius, intensity) {
             /* Lightens an area of the canvas using a gradient. */
-            ctx.save();
-            ctx.globalAlpha = intensity;
-            ctx.globalCompositeOperation = 'lighter';
-            var rnd = 0.05 * Math.sin(1.1 * Date.now() / 1000);
-            radius = radius * (1 + rnd);
-            var radialGradient = ctx.createRadialGradient(x, y, 0, x, y, radius);
-            radialGradient.addColorStop(0.0, '#BB9');
-            radialGradient.addColorStop(0.2 + rnd, '#AA8');
-            radialGradient.addColorStop(0.7 + rnd, '#330');
-            radialGradient.addColorStop(0.90, '#110');
-            radialGradient.addColorStop(1, '#000');
-            ctx.fillStyle = radialGradient;
-            ctx.beginPath();
-            ctx.arc(x, y, radius, 0, 2 * Math.PI);
-            ctx.fill();
-            ctx.globalAlpha = 1;
-            ctx.restore();
+            
+            // check if arguments are finite
+            if (isFinite(x) && isFinite(y) && isFinite(radius)) {
+                ctx.save();
+                ctx.globalAlpha = intensity;
+                ctx.globalCompositeOperation = 'lighter';
+                var rnd = 0.05 * Math.sin(1.1 * Date.now() / 1000);
+                radius = radius * (1 + rnd);
+                var radialGradient = ctx.createRadialGradient(x, y, 0, x, y, radius);
+                radialGradient.addColorStop(0.0, '#BB9');
+                radialGradient.addColorStop(0.2 + rnd, '#AA8');
+                radialGradient.addColorStop(0.7 + rnd, '#330');
+                radialGradient.addColorStop(0.90, '#110');
+                radialGradient.addColorStop(1, '#000');
+                ctx.fillStyle = radialGradient;
+                ctx.beginPath();
+                ctx.arc(x, y, radius, 0, 2 * Math.PI);
+                ctx.fill();
+                ctx.globalAlpha = 1;
+                ctx.restore();
+            }
         }
     };
 }());
