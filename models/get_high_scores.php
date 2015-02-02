66b7412f9eb1052a81aa7c7507295ec92d239917
Chrissan Sy <sy.chrissan@gmail.com>
2015-02-02 08:42:25 +0800

    fixed stray <?php in get_high_scores.php

--- models/get_high_scores.php
+++ models/get_high_scores.php
@@ -3,7 +3,6 @@
 
     // check for ajax
     if(isset($_POST['ajax'])) {
-        <?php
         // connect to db
         $conn_string = "host=ec2-174-129-1-179.compute-1.amazonaws.com port=5432 dbname=d6e1frjth87pfd user=cizbwdxggeralb password=Vxbh0ac32QujliNdiV74wBe11N sslmode=require";
         $conn = pg_connect($conn_string);
