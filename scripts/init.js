96806078ec40838f9ecf60e53d145934b3ea4c20
Chrissan Sy <sy.chrissan@gmail.com>
2017-02-08 02:41:05 +0800

    ...

--- scripts/init.js
+++ scripts/init.js
@@ -1,8 +1,3 @@
 /* Namespace declarations and other initialization values */
 
 var BattleTanks = BattleTanks || {};
-
-var browser = navigator.userAgent.toLowerCase();
-if (browser.indexOf('firefox') === -1) {
-    document.getElementById('mozfox-msg').style.display = 'block';
-}
